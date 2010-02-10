63c4c5d5718b667e0ea84ee9fa7afe4497f0609a
Rene Milk <rene.milk@uni-muenster.de>
2010-02-10 16:33:16 +0100

    implement new Boundary surface class

--- gridhelper.py
+++ gridhelper.py
@@ -30,7 +30,7 @@ class DimensionIncompatibleException(Exception):
 class ImpossibleException(Exception):
 	pass
 
-import math
+import math, copy
 from euclid import *
 
 class PLCPointList:
@@ -48,6 +48,7 @@ class PLCPointList:
 		else:
 			glob_idx = PLCPointList.global_vertices.index(x)
 		self.verts[glob_idx] = x
+		assert isinstance( glob_idx, int )
 		return glob_idx
 
 	def simpleString(self):
@@ -58,8 +59,11 @@ class PLCPointList:
 
 class Simplex:
 	def __init__(self,v1,v2,v3):
+		assert isinstance( v1, int )
 		self.v1 = v1
+		assert isinstance( v2, int )
 		self.v2 = v2
+		assert isinstance( v3, int )
 		self.v3 = v3
 
 	def __repr__(self):
@@ -127,8 +131,8 @@ class FullGrid:
 				f1 = self.rings[-2]
 			else:
 				f1 = self.fans[-1]
-		elif isinstance( new_f, FanningSimplexList ):
-			print 'connecting FanningSimplexList\n'
+		elif isinstance( new_f, BoundarySurface ):
+			print 'connecting BoundarySurface\n'
 			self.fans.append( new_f )
 			if len(self.rings) > 0:
 				f1 = self.rings[-1]
@@ -138,7 +142,6 @@ class FullGrid:
 		if len(f1.vertex_idx) !=  len(f2.vertex_idx):
 			raise DimensionIncompatibleException()
 		b_len = len(f1.vertex_idx)
-		print b_len
 		for i in range ( 0, b_len  ):
 			self.connecting_simplices.append( \
 				Simplex(	f1.vertex_idx[i-1],
@@ -182,7 +185,9 @@ class FullGrid:
 		out.write( '%d 1\n'%(boundary_segment_count) )
 		cBseg = 0
 		for f in self.fans:
+			assert isinstance( f, BoundarySurface )
 			for b1 in f.simplices:
+				assert isinstance( b1, Simplex )
 				out.write( '%d %d %d %d %d\n'%(3,b1.v1,b1.v2,b1.v3,f.boundaryId)  )
 				cBseg += 1
 		for c in self.connecting_simplices:
@@ -190,8 +195,61 @@ class FullGrid:
 			cBseg += 1
 		out.write( '%d\n'%(0))
 
+class BoundarySurface:
+	def __init__(self, mid, first, count_inner_rings, count_spokes, bid ):
+		self.mid 				= mid
+		self.first 				= first
+		self.count_inner_rings 	= count_inner_rings
+		self.count_spokes 		= count_spokes
+		self.boundaryId 		= bid
+		self.alpha				= math.radians( 360. / (self.count_spokes ) )
+		self.alpha_half			= self.alpha / 2.0
 
+		self.simplices				= []	
+		self.outer_vertices_idx 	= []
+		self.inner_vertices_idx 	= []
+		self.vertices 				= PLCPointList( 3 )
+		self.spokes					= []
+		self.mid_idx 				= self.vertices.appendVert( self.mid )
+		
+		rot_mat = Matrix4.new_rotatez( self.alpha )
+		
+		L = Vector3(self.first.x, self.first.y, self.first.z)
+		z_offset = Vector3(0, 0, self.first.z)
+		for i in range( 0, self.count_spokes  ):
+			if i != 0:
+				L -= z_offset
+				L = rot_mat * L
+				L += z_offset
+			L_idx = self.vertices.appendVert( L )
+			self.outer_vertices_idx.append( L_idx )
+			spoke_direction = self.mid - L 
+			assert self.mid.z == L.z
+			spoke_vertex_idx = []
+			spoke_vertex_idx.append( L_idx )
+			spoke_add  = spoke_direction / float( self.count_inner_rings )
+			for i in range( 1, self.count_inner_rings ):
+				p = L + ( i * spoke_add )
+				spoke_vertex_idx.append( self.vertices.appendVert( p ) )
+			self.inner_vertices_idx.append( spoke_vertex_idx[-1] )
+			self.spokes.append( spoke_vertex_idx )
 
+		for i in range( 0, self.count_spokes ):
+			current_spoke = self.spokes[i-1]
+			left_spoke = self.spokes[i-2]
+			right_spoke = self.spokes[i]
+			for j in range( 0, self.count_inner_rings - 1 ):
+				#print 'current spoke j,j+1, left spoke j: %f - %f - %f '%(current_spoke[j], current_spoke[j+1], left_spoke[j])
+				self.simplices.append( Simplex( current_spoke[j], current_spoke[j+1], left_spoke[j] ) )
+				self.simplices.append( Simplex( current_spoke[j], current_spoke[j+1], right_spoke[j+1] ) )
+
+		for i in range( 0, len(self.inner_vertices_idx)  ):
+			self.simplices.append( Simplex( self.inner_vertices_idx[i], self.inner_vertices_idx[i-1], self.mid_idx ) )
+
+		#this is exposed to FullGrid
+		self.vertex_idx = self.outer_vertices_idx
+
+		
 
 
 
--- tube.py
+++ tube.py
@@ -54,7 +54,10 @@ parser.add_option("-n", "--num_verts", dest="num_verts", default=6,
                   help="number of vertices in circle approx", type='int')
 
 parser.add_option("-m", "--num_midrings", dest="num_midrings", default=0,
-                  help="number midrings inserted between boundary faces", type='int')
+                  help="number of midrings inserted between boundary faces", type='int')
+
+parser.add_option("-s", "--num_spoke_points", dest="num_spoke_points", default=2,
+                  help="number of spoke points on boundary faces", type='int')
 
 parser.add_option("-f", "--filename", dest="filename", default='out.smesh',
                   help="output filename", type='string')
@@ -65,8 +68,10 @@ area 			= options.area
 tube_length		= options.tube_length
 num_verts 		= options.num_verts
 alpha			= math.radians( 360. / (num_verts ) )
-alpha_half		= alpha / 2.
+alpha_half		= alpha / 2.0
 num_midrings	= int(options.num_midrings)
+num_spoke_points = options.num_spoke_points
+assert num_spoke_points > 0
 area_one_tri = area * math.pi / float(num_verts)
 L_x = math.sqrt( area_one_tri / ( math.sin( alpha_half ) * math.cos( alpha_half ) ) )
 if use_hyperbole_functor:
@@ -77,19 +82,15 @@ else:
 	print "using identity functor"
 
 """left boundary area"""
+
 origin_L = Vector3(0,0,0)
-points_L = PLCPointList( 3 )
-bound_L = FanningSimplexList( points_L.appendVert( origin_L ), 2 )
 L = functor.scale( Vector3( L_x, 0, 0 ) )
-bound_L.addVertex( points_L.appendVert( L ) )
-rot_mat = Matrix4.new_rotatez( alpha )
-for i in range( 1, num_verts  ):
-	L = rot_mat * L
-	bound_L.addVertex(points_L.appendVert( L ))
-bound_L.close()
+bound_L = BoundarySurface( origin_L, L, num_spoke_points, num_verts, 2 )
+
 grid = FullGrid( bound_L, 4 )
 
 """ midrings, if num > 0 """
+rot_mat = Matrix4.new_rotatez( alpha )
 incr = tube_length / float( num_midrings + 1 )
 for i in range( 1, num_midrings + 1 ):
 	points_M = PLCPointList( 3 )
@@ -103,16 +104,10 @@ for i in range( 1, num_midrings + 1 ):
 
 """right boundary"""
 kipp_mat = Matrix4.new_rotatey( math.radians(15) )
+
 origin_R = Vector3(0,0,tube_length )
-points_R = PLCPointList( 3 )
-bound_R = FanningSimplexList( points_R.appendVert( origin_R ), 3 )
 R = functor.scale( Vector3( L_x, 0, tube_length ) )
-#R = kipp_mat * R #skalier groesser R
-bound_R.addVertex( points_R.appendVert( R ) )
-for i in range( 1, num_verts  ):
-	R = rot_mat  * R
-	bound_R.addVertex(points_R.appendVert( R ))
-bound_R.close()
+bound_R = BoundarySurface( origin_R, R, num_spoke_points, num_verts, 3 )
 
 grid.connect(bound_R)
 grid.outputPLC( options.filename, sys.argv, options )
