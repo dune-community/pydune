5f3cddd2c1d6bee8b15ddf251b22c08d0eec450d
Rene Milk <rene.milk@uni-muenster.de>
2012-07-06 12:01:09 +0200

    adds dunecontrol wrapper

---
+++ dune/control.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import tempfile
+import os
+import random
+import string
+import subprocess
+
+class Dunecontrol(object):
+	def __init__(self,script_path):
+		self._script_path = script_path
+		self._base_dir = os.path.join(os.path.dirname(script_path), '../..')
+
+	def _call(self, args):
+		try:
+			cl = [self._script_path] + args
+		except:
+			cl = [self._script_path] + args.split()
+		return subprocess.check_output(cl, cwd=self._base_dir,stderr=subprocess.STDOUT)
+
+	def printdeps(self, module):
+		out = self._call(['--module=%s'%module, 'printdeps']).split('\n')[1:]
+		required = [ m.split()[0].strip() for m in out if 'required' in m ]
+		suggested = [ m.split()[0].strip() for m in out if 'suggested' in m ]
+		return (required, suggested)
+
+	def configure(self,module=None):
+		return self._call(['--module=%s'%module,'configure']).split()
+
+	def autogen(self,module=None):
+		return self._call(['--module=%s'%module,'autogen']).split()
+
+	def make(self,module=None):
+		return self._call(['--module=%s'%module,'make']).split()
+
+if __name__ == "__main__":
+	p = '/home/r_milk01/projekte/uni/dune/multiscale-super/dune-common/bin/dunecontrol'
+	ctrl = Dunecontrol(p)
+	print ctrl.printdeps('dune-stuff')
+	
