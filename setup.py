89b07304c11a599f48ce4e3c0dd1d64cf4e3230c
Rene Milk <rene.milk@uni-muenster.de>
2012-10-02 16:00:24 +0200

    setup.py dependency fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = 'pyDune',
-    version = '0.1.3',
+    version = '0.1.3a',
     author = 'Rene Milk',
     author_email = 'rene.milk@uni-muenster.de',
     packages = ['dune', 'dune.mesh', 'dune.mesh.gui', 'dune.mesh.dgf', 'dune.mesh.smesh',
@@ -14,7 +14,8 @@ setup(
     long_description = open('README.txt').read(),
     # running `setup.py sdist' gives a warning about this, but still
     # install_requires is the only thing that works with pip/easy_install...
-    install_requires = ['matplotlib', 'pyparsing', 'nose-cov', 'nosetest', 'GitPython'],
+    # we do not list pyqt here since pip can't seem to install it
+    install_requires = ['matplotlib', 'pyparsing', 'nose-cov', 'nose', 'nosehtmloutput', 'rednose', 'GitPython'],
     classifiers = ['Development Status :: 4 - Beta',
         'Environment :: X11 Applications :: Qt',
         'Intended Audience :: Science/Research',
