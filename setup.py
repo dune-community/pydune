5d4932126812f0ec4cc1a3029ee3b4a385183d3b
Rene Milk <rene.milk@uni-muenster.de>
2012-10-03 16:40:07 +0200

    setup fixup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import sys
 from distutils.core import setup
 
+tests_require = ['nose-cov', 'nose', 'nosehtmloutput', 'rednose']
+
 setup(
     name = 'pyDune',
     version = '0.1.4',
@@ -12,10 +14,11 @@ setup(
     url = 'http://users.dune-project.org/projects/pydune',
     description = 'assorrted DUNE related scripts and utilities',
     long_description = open('README.txt').read(),
+    tests_require = tests_require,
     # running `setup.py sdist' gives a warning about this, but still
     # install_requires is the only thing that works with pip/easy_install...
     # we do not list pyqt here since pip can't seem to install it
-    install_requires = ['matplotlib', 'pyparsing', 'nose-cov', 'nose', 'nosehtmloutput', 'rednose', 'GitPython'],
+    install_requires = ['matplotlib', 'pyparsing', 'GitPython'] + tests_require,
     classifiers = ['Development Status :: 4 - Beta',
         'Environment :: X11 Applications :: Qt',
         'Intended Audience :: Science/Research',
